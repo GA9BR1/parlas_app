abbe36b07a4ebe0454ea09fa3a596491ea2a70d7
gustavo.costa <gustavoalberttodev@gmail.com>
2024-01-16 13:57:17 -0300

    Adição de descrição, botão de like e envio de mensagens

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:like_button/like_button.dart';
+import 'package:project_study/users_request.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late Future<List<Person>> people;
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      people = getPeople();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,66 +65,44 @@ class HomeScreen extends StatelessWidget {
         ),
         body: SafeArea(
           child: Center(
-            child: Column(
-              children: [
-                SizedBox(height: MediaQuery.of(context).size.height * .031),
-                Container(
-                  width: MediaQuery.of(context).size.width * .9,
-                  height: MediaQuery.of(context).size.height * .7,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    image: const DecorationImage(
-                      image: AssetImage("lib/images/aleatory_person.jpeg"),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  child: Container(
-                    margin: EdgeInsets.only(
-                        top: MediaQuery.of(context).size.height * .5),
-                    decoration: BoxDecoration(
-                      borderRadius: const BorderRadius.only(
-                          bottomLeft: Radius.circular(20),
-                          bottomRight: Radius.circular(20)),
-                      gradient: LinearGradient(
-                        begin: Alignment.bottomLeft,
-                        end: Alignment.bottomRight,
-                        colors: [
-                          Colors.black.withOpacity(0.4),
-                          Colors.transparent
-                        ],
-                      ),
-                    ),
-                    child: Container(
-                      padding: EdgeInsets.all(
-                          MediaQuery.of(context).size.width * .03),
-                      child: Column(
-                        children: [
-                          Row(
-                            children: [
-                              Text("José Hernandez",
-                                  style: GoogleFonts.lobster(
-                                      fontSize: 20, color: Colors.white)),
-                              const SizedBox(width: 10),
-                              Text("20",
-                                  style: GoogleFonts.lobster(
-                                      fontSize: 20, color: Colors.white)),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              Text("Mexico - Mexico City",
-                                  style: GoogleFonts.quicksand(
-                                      fontSize: 15, color: Colors.white)),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
+              child: FutureBuilder(
+            future: getPeople(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              } else {
+                if (snapshot.data != null && snapshot.data!.isNotEmpty) {
+                  return PageView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      for (var person in snapshot.data as List<Person>) ...[
+                        Container(
+                            padding: EdgeInsets.symmetric(
+                                horizontal:
+                                    MediaQuery.of(context).size.width * .05,
+                                vertical:
+                                    MediaQuery.of(context).size.height * .02),
+                            child: PersonCard(person: person)),
+                      ]
+                    ],
+                  );
+                } else if (snapshot.hasError) {
+                  return Text("${snapshot.error}");
+                }
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Server is not responding as expected...",
+                        style: GoogleFonts.nunito(
+                            fontSize: 20, color: Colors.white)),
+                    Text("Try again later.",
+                        style: GoogleFonts.nunito(
+                            fontSize: 20, color: Colors.white)),
+                  ],
+                );
+              }
+            },
+          )),
         ),
         bottomNavigationBar: BottomAppBar(
             height: MediaQuery.of(context).size.height * .15,
@@ -138,6 +133,112 @@ class HomeScreen extends StatelessWidget {
                 ],
               ),
             )),
+      ),
+    );
+  }
+}
+
+class PersonCard extends StatelessWidget {
+  final Person person;
+
+  const PersonCard({
+    super.key,
+    required this.person,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * .9,
+      height: MediaQuery.of(context).size.height * .7,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        image: DecorationImage(
+          image: Image.network(person.picture.large).image,
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Container(
+        margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * .45),
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.only(
+              bottomLeft: Radius.circular(20),
+              bottomRight: Radius.circular(20)),
+          gradient: LinearGradient(
+            begin: Alignment.bottomLeft,
+            end: Alignment.bottomRight,
+            colors: [Colors.black.withOpacity(0.4), Colors.transparent],
+          ),
+        ),
+        child: Container(
+          padding: EdgeInsets.only(
+              top: MediaQuery.of(context).size.width * .03,
+              left: MediaQuery.of(context).size.width * .03,
+              right: MediaQuery.of(context).size.width * .03),
+          child: Column(children: [
+            Row(
+              children: [
+                Text("${person.name.first} ${person.name.last}",
+                    style:
+                        GoogleFonts.lobster(fontSize: 20, color: Colors.white)),
+                const SizedBox(width: 10),
+                Text("${person.dob.age}",
+                    style:
+                        GoogleFonts.lobster(fontSize: 20, color: Colors.white)),
+              ],
+            ),
+            Row(
+              children: [
+                Text("${person.location.country} - ${person.location.city}",
+                    style: GoogleFonts.quicksand(
+                        fontSize: 15, color: Colors.white)),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    "lorem ipsum dolor sit amet, consectetur adipiscing elit"
+                    "Ut auctor, nisl eget ultricies ultrices, nisl nisl"
+                    "molestie nisl, nec ultricies nisl nisl eget ultricies ultrices, nisl nisl",
+                    style: GoogleFonts.quicksand(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 15,
+                        color: Colors.white),
+                    maxLines: 3,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 5),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                    horizontal: MediaQuery.of(context).size.width * .02,
+                    vertical: MediaQuery.of(context).size.height * .005),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.black.withOpacity(0.4)),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                        onPressed: () => {},
+                        icon: const Icon(
+                          Icons.send,
+                          color: Colors.white,
+                          size: 32,
+                        )),
+                    const LikeButton(size: 35),
+                  ],
+                ),
+              ),
+            ),
+          ]),
+        ),
       ),
     );
   }
---
+++ lib/users_request.dart
@@ -0,0 +1,156 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+Future<List<Person>> getPeople() async {
+  var url = Uri.parse("https://randomuser.me/api/?results=10");
+  try {
+    var response = await http.get(url);
+
+    return peopleFromJson(response.body);
+  } catch (e) {
+    return [];
+  }
+}
+
+List<Person> peopleFromJson(String str) {
+  final jsonData = json.decode(str);
+  final results = jsonData['results'];
+  List<Person> people = [];
+  for (var result in results) {
+    people.add(Person.fromJson(result));
+  }
+  return people;
+}
+
+class Person {
+  late String gender;
+  late Name name;
+  late Location location;
+  late String email;
+  late Dob dob;
+  late String cell;
+  late Picture picture;
+
+  Person({
+    required this.gender,
+    required this.name,
+    required this.location,
+    required this.email,
+    required this.dob,
+    required this.cell,
+    required this.picture,
+  });
+
+  factory Person.fromJson(Map<String, dynamic> result) {
+    return Person(
+      gender: result['gender'],
+      name: Name.fromJson(result['name']),
+      location: Location.fromJson(result['location']),
+      email: result['email'],
+      dob: Dob.fromJson(result['dob']),
+      cell: result['cell'],
+      picture: Picture.fromJson(result['picture']),
+    );
+  }
+}
+
+class Name {
+  late String title;
+  late String first;
+  late String last;
+
+  Name({
+    required this.title,
+    required this.first,
+    required this.last,
+  });
+
+  factory Name.fromJson(Map<String, dynamic> name) {
+    return Name(
+      title: name['title'],
+      first: name['first'],
+      last: name['last'],
+    );
+  }
+}
+
+class Dob {
+  late String date;
+  late int age;
+
+  Dob({
+    required this.date,
+    required this.age,
+  });
+
+  factory Dob.fromJson(Map<String, dynamic> dob) {
+    return Dob(
+      date: dob['date'],
+      age: dob['age'],
+    );
+  }
+}
+
+class Picture {
+  late String large;
+  late String medium;
+  late String thumbnail;
+
+  Picture({
+    required this.large,
+    required this.medium,
+    required this.thumbnail,
+  });
+
+  factory Picture.fromJson(Map<String, dynamic> picture) {
+    return Picture(
+      large: picture['large'],
+      medium: picture['medium'],
+      thumbnail: picture['thumbnail'],
+    );
+  }
+}
+
+class Location {
+  late Street street;
+  late String city;
+  late String state;
+  late String country;
+  late String postcode;
+
+  Location({
+    required this.street,
+    required this.city,
+    required this.state,
+    required this.country,
+    required this.postcode,
+  });
+
+  factory Location.fromJson(Map<String, dynamic> location) {
+    return Location(
+      street: Street.fromJson(location['street']),
+      city: location['city'],
+      state: location['state'],
+      country: location['country'],
+      postcode: location['postcode'].toString(),
+    );
+  }
+}
+
+class Street {
+  late int number;
+  late String name;
+
+  Street({
+    required this.number,
+    required this.name,
+  });
+
+  factory Street.fromJson(Map<String, dynamic> street) {
+    return Street(
+      number: street['number'],
+      name: street['name'],
+    );
+  }
+}
