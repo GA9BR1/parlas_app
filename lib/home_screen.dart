181d923a9040a98733eac376b244f3b8f680bb96
gustavo.costa <gustavoalberttodev@gmail.com>
2024-01-16 15:52:17 -0300

    Abertura de descrição e refresh com movimento vertical de cima para baixo.

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -39,112 +39,131 @@ class _HomeScreenState extends State<HomeScreen> {
           Color.fromARGB(174, 230, 53, 9),
         ],
       )),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        appBar: AppBar(
+      child: GestureDetector(
+        onVerticalDragEnd: (details) => {
+          if (details.primaryVelocity! > 0)
+            {
+              setState(() {
+                people = getPeople();
+              })
+            }
+        },
+        child: Scaffold(
           backgroundColor: Colors.transparent,
-          actions: const [
-            Align(
-              alignment: Alignment.centerRight,
-              child: Icon(
-                Icons.messenger_outline_outlined,
-                color: Colors.white,
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            actions: const [
+              Align(
+                alignment: Alignment.centerRight,
+                child: Icon(
+                  Icons.messenger_outline_outlined,
+                  color: Colors.white,
+                ),
               ),
-            ),
-            SizedBox(width: 10),
-          ],
-          title: Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                "Parlas",
-                style: GoogleFonts.pacifico(
-                    fontSize: 25,
-                    fontWeight: FontWeight.normal,
-                    color: Colors.white),
-              )),
-        ),
-        body: SafeArea(
-          child: Center(
-              child: FutureBuilder(
-            future: getPeople(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const CircularProgressIndicator();
-              } else {
-                if (snapshot.data != null && snapshot.data!.isNotEmpty) {
-                  return PageView(
-                    scrollDirection: Axis.horizontal,
+              SizedBox(width: 10),
+            ],
+            title: Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Parlas",
+                  style: GoogleFonts.pacifico(
+                      fontSize: 25,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.white),
+                )),
+          ),
+          body: SafeArea(
+            child: Center(
+                child: FutureBuilder(
+              future: getPeople(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const CircularProgressIndicator();
+                } else {
+                  if (snapshot.data != null && snapshot.data!.isNotEmpty) {
+                    return PageView(
+                      physics: const BouncingScrollPhysics(),
+                      scrollDirection: Axis.horizontal,
+                      children: [
+                        for (var person in snapshot.data as List<Person>) ...[
+                          Container(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal:
+                                      MediaQuery.of(context).size.width * .05,
+                                  vertical:
+                                      MediaQuery.of(context).size.height * .02),
+                              child: PersonCard(person: person)),
+                        ]
+                      ],
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text("${snapshot.error}");
+                  }
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      for (var person in snapshot.data as List<Person>) ...[
-                        Container(
-                            padding: EdgeInsets.symmetric(
-                                horizontal:
-                                    MediaQuery.of(context).size.width * .05,
-                                vertical:
-                                    MediaQuery.of(context).size.height * .02),
-                            child: PersonCard(person: person)),
-                      ]
+                      Text("Server is not responding as expected...",
+                          style: GoogleFonts.nunito(
+                              fontSize: 20, color: Colors.white)),
+                      Text("Try again later.",
+                          style: GoogleFonts.nunito(
+                              fontSize: 20, color: Colors.white)),
                     ],
                   );
-                } else if (snapshot.hasError) {
-                  return Text("${snapshot.error}");
                 }
-                return Column(
+              },
+            )),
+          ),
+          bottomNavigationBar: BottomAppBar(
+              height: MediaQuery.of(context).size.height * .15,
+              surfaceTintColor: Colors.transparent,
+              color: Colors.transparent,
+              padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * .02,
+                  left: MediaQuery.of(context).size.width * .35,
+                  right: MediaQuery.of(context).size.width * .35,
+                  bottom: MediaQuery.of(context).size.height * .04),
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text("Server is not responding as expected...",
-                        style: GoogleFonts.nunito(
-                            fontSize: 20, color: Colors.white)),
-                    Text("Try again later.",
-                        style: GoogleFonts.nunito(
-                            fontSize: 20, color: Colors.white)),
+                    IconButton(
+                        onPressed: () => context.go('/settings'),
+                        icon: const Icon(
+                          Icons.settings,
+                          size: 30,
+                        )),
+                    IconButton(
+                      onPressed: () => {},
+                      icon: const Icon(Icons.person, size: 30),
+                    ),
                   ],
-                );
-              }
-            },
-          )),
+                ),
+              )),
         ),
-        bottomNavigationBar: BottomAppBar(
-            height: MediaQuery.of(context).size.height * .15,
-            surfaceTintColor: Colors.transparent,
-            color: Colors.transparent,
-            padding: EdgeInsets.only(
-                top: MediaQuery.of(context).size.height * .02,
-                left: MediaQuery.of(context).size.width * .35,
-                right: MediaQuery.of(context).size.width * .35,
-                bottom: MediaQuery.of(context).size.height * .04),
-            child: Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20), color: Colors.white),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                      onPressed: () => context.go('/settings'),
-                      icon: const Icon(
-                        Icons.settings,
-                        size: 30,
-                      )),
-                  IconButton(
-                    onPressed: () => {},
-                    icon: const Icon(Icons.person, size: 30),
-                  ),
-                ],
-              ),
-            )),
       ),
     );
   }
 }
 
-class PersonCard extends StatelessWidget {
+class PersonCard extends StatefulWidget {
   final Person person;
 
   const PersonCard({
     super.key,
     required this.person,
   });
+
+  @override
+  State<PersonCard> createState() => _PersonCardState();
+}
+
+class _PersonCardState extends State<PersonCard> {
+  bool openDescription = false;
 
   @override
   Widget build(BuildContext context) {
@@ -154,90 +173,141 @@ class PersonCard extends StatelessWidget {
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(20),
         image: DecorationImage(
-          image: Image.network(person.picture.large).image,
+          image: Image.network(widget.person.picture.large).image,
           fit: BoxFit.cover,
         ),
       ),
-      child: Container(
-        margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * .45),
-        decoration: BoxDecoration(
-          borderRadius: const BorderRadius.only(
-              bottomLeft: Radius.circular(20),
-              bottomRight: Radius.circular(20)),
-          gradient: LinearGradient(
-            begin: Alignment.bottomLeft,
-            end: Alignment.bottomRight,
-            colors: [Colors.black.withOpacity(0.4), Colors.transparent],
+      child: GestureDetector(
+        onVerticalDragStart: (_) => {
+          setState(() {
+            openDescription = !openDescription;
+          })
+        },
+        onTap: () => {
+          setState(() {
+            openDescription = !openDescription;
+          })
+        },
+        child: AnimatedContainer(
+          curve: Curves.easeInOutCubicEmphasized,
+          duration: const Duration(milliseconds: 200),
+          margin: EdgeInsets.only(
+              top: MediaQuery.of(context).size.height *
+                  (openDescription ? .0 : 0.43)),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              bottomLeft: const Radius.circular(20),
+              bottomRight: const Radius.circular(20),
+              topLeft:
+                  openDescription ? const Radius.circular(20) : Radius.zero,
+              topRight:
+                  openDescription ? const Radius.circular(20) : Radius.zero,
+            ),
+            gradient: LinearGradient(
+              begin: Alignment.bottomLeft,
+              end: Alignment.bottomRight,
+              colors: [Colors.black.withOpacity(0.4), Colors.transparent],
+            ),
           ),
-        ),
-        child: Container(
-          padding: EdgeInsets.only(
-              top: MediaQuery.of(context).size.width * .03,
-              left: MediaQuery.of(context).size.width * .03,
-              right: MediaQuery.of(context).size.width * .03),
-          child: Column(children: [
-            Row(
-              children: [
-                Text("${person.name.first} ${person.name.last}",
-                    style:
-                        GoogleFonts.lobster(fontSize: 20, color: Colors.white)),
-                const SizedBox(width: 10),
-                Text("${person.dob.age}",
-                    style:
-                        GoogleFonts.lobster(fontSize: 20, color: Colors.white)),
-              ],
-            ),
-            Row(
-              children: [
-                Text("${person.location.country} - ${person.location.city}",
-                    style: GoogleFonts.quicksand(
-                        fontSize: 15, color: Colors.white)),
-              ],
-            ),
-            const SizedBox(height: 10),
-            Row(
-              children: [
-                Expanded(
-                  child: Text(
-                    "lorem ipsum dolor sit amet, consectetur adipiscing elit"
-                    "Ut auctor, nisl eget ultricies ultrices, nisl nisl"
-                    "molestie nisl, nec ultricies nisl nisl eget ultricies ultrices, nisl nisl",
-                    style: GoogleFonts.quicksand(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 15,
-                        color: Colors.white),
-                    maxLines: 3,
-                    overflow: TextOverflow.ellipsis,
+          child: Container(
+            padding: EdgeInsets.only(
+                top: MediaQuery.of(context).size.width * .03,
+                left: MediaQuery.of(context).size.width * .03,
+                right: MediaQuery.of(context).size.width * .03),
+            child: Column(children: [
+              Row(
+                children: [
+                  Text("${widget.person.name.first} ${widget.person.name.last}",
+                      style: GoogleFonts.lobster(
+                          fontSize: 20, color: Colors.white)),
+                  const SizedBox(width: 10),
+                  Text("${widget.person.dob.age}",
+                      style: GoogleFonts.lobster(
+                          fontSize: 20, color: Colors.white)),
+                ],
+              ),
+              Row(
+                children: [
+                  Text(
+                      "${widget.person.location.country} - ${widget.person.location.city}",
+                      style: GoogleFonts.quicksand(
+                          fontSize: 15, color: Colors.white)),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Row(
+                children: [
+                  Expanded(
+                    child: AnimatedSize(
+                      curve: Curves.easeInOutCubicEmphasized,
+                      duration: const Duration(milliseconds: 200),
+                      child: Text(
+                        "lorem ipsum dolor sit amet, consectetur adipiscing elit"
+                        "Ut auctor, nisl eget ultricies ultrices, nisl nisl"
+                        "molestie nisl, nec ultricies nisl nisl eget ultricies ultrices, nisl nisl"
+                        "molestie nisl, nec ultricies nisl nisl eget ultricies ultrices, nisl nisl"
+                        "molestie nisl, nec ultricies nisl nisl eget ultricies ultrices, nisl nisl"
+                        "molestie nisl, nec ultricies nisl nisl eget ultricies ultrices, nisl nisl"
+                        "molestie nisl, nec ultricies nisl nisl eget ultricies ultrices, nisl nisl"
+                        "molestie nisl, nec ultricies nisl nisl eget ultricies ultrices, nisl nisl"
+                        "molestie nisl, nec ultricies nisl nisl eget ultricies ultrices, nisl nisl"
+                        "molestie nisl, nec ultricies nisl nisl eget ultricies ultrices, nisl nisl"
+                        "molestie nisl, nec ultricies nisl nisl eget ultricies ultrices, nisl nisl"
+                        "molestie nisl, nec ultricies nisl",
+                        style: GoogleFonts.quicksand(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 15,
+                            color: Colors.white),
+                        maxLines: openDescription ? 25 : 3,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 5),
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.only(
+                      bottom: MediaQuery.of(context).size.height * .01),
+                  alignment: Alignment.bottomLeft,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Container(
+                            padding: EdgeInsets.symmetric(
+                                horizontal:
+                                    MediaQuery.of(context).size.width * .02,
+                                vertical:
+                                    MediaQuery.of(context).size.height * .005),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: Colors.black.withOpacity(0.4)),
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                IconButton(
+                                    onPressed: () => {},
+                                    icon: const Icon(
+                                      Icons.send,
+                                      color: Colors.white,
+                                      size: 32,
+                                    )),
+                                const LikeButton(size: 35),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
-              ],
-            ),
-            const SizedBox(height: 5),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Container(
-                padding: EdgeInsets.symmetric(
-                    horizontal: MediaQuery.of(context).size.width * .02,
-                    vertical: MediaQuery.of(context).size.height * .005),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.black.withOpacity(0.4)),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    IconButton(
-                        onPressed: () => {},
-                        icon: const Icon(
-                          Icons.send,
-                          color: Colors.white,
-                          size: 32,
-                        )),
-                    const LikeButton(size: 35),
-                  ],
-                ),
               ),
-            ),
-          ]),
+            ]),
+          ),
         ),
       ),
     );
